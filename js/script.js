11d7158c53ce8d7f7deead1d7a15cf833d7af70f
NT Phuong Thao <thao.tp.nguyen02@gmail.com>
2023-02-15 16:59:15 -0500

    Fixed the sun/moon icon when toggle theme and add logics to hide/show menu when displayed in mobiles

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // DARK THEME IS WORKING
 const themeButton = document.getElementById('theme-button');
 const darkTheme = 'dark-theme';
-const icon = 'uil-sun';
+const iconTheme = 'uil-sun';
 
 // PREVIOUSLY SELECTED TOPIC (checking from local storage)
 const selectTheme = localStorage.getItem('selected-theme')
@@ -28,4 +28,33 @@ themeButton.addEventListener('click', () => {
   localStorage.setItem('selected-icon', detCurrentIcon());
 });
 
-console.log('MY THEME SETTING IS WORKING');
+// MENU SHOW Y HIDDEN
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
+
+// MENU SHOW 
+// Validate if the constant exists
+if (navToggle) {
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu');
+  })
+}
+
+if (navClose) {
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu');
+  })
+}
+
+
+// MENU REMOVE
+const navLink = document.querySelectorAll('.nav__link');
+
+function linkAction() {
+  const navMenu = document.getElementById('nav-menu');
+  navMenu.classList.remove('show-menu');
+}
+
+navLink.forEach(n => n.addEventListener('click', linkAction));
+
